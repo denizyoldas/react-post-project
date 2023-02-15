a33dd24bf20d266264d3ce46029189195ca7c198
denizyoldas <daksu30@gmail.com>
2023-02-16 01:23:18 +0300

    user detail card done

--- src/components/user-detail-card.tsx
+++ src/components/user-detail-card.tsx
@@ -6,9 +6,70 @@ interface Props {
 }
 
 export default function UserDetailCard({ user }: Props) {
+  const {
+    id,
+    title,
+    firstName,
+    lastName,
+    gender,
+    email,
+    dateOfBirth,
+    registerDate,
+    phone,
+    picture,
+    location
+  } = user || {}
+
   return (
     <div className="overflow-hidden rounded-lg bg-white shadow-lg">
-      {user?.firstName}
+      <div className="flex items-center justify-between px-6 py-4">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            <img
+              className="h-12 w-12 rounded-full"
+              src={picture}
+              alt="avatar"
+            />
+          </div>
+          <div className="ml-3">
+            <p className="text-sm font-medium leading-5 text-gray-900">
+              {firstName} {lastName}
+            </p>
+            <p className="text-sm leading-5 text-gray-500">{email}</p>
+          </div>
+        </div>
+      </div>
+      <div className="px-6 py-4">
+        <div className="text-sm leading-5 text-gray-900">
+          <p className="mb-2">
+            <span className="font-medium">Phone:</span> {phone}
+          </p>
+          <p className="mb-2">
+            <span className="font-medium">ID:</span> {id}
+          </p>
+          <p className="mb-2">
+            <span className="font-medium">Title:</span> {title}
+          </p>
+          <p className="mb-2">
+            <span className="font-medium">Gender:</span> {gender}
+          </p>
+          <p className="mb-2">
+            <span className="font-medium">Date of Birth:</span> {dateOfBirth}
+          </p>
+          <p className="mb-2">
+            <span className="font-medium">Register Date:</span> {registerDate}
+          </p>
+          <p className="mb-2">
+            {Object.entries(location).map(([key, value]) => (
+              <span key={key}>
+                <>
+                  <span className="font-medium">{key}:</span> {value}
+                </>
+              </span>
+            ))}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
